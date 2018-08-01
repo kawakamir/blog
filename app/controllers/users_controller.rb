827d9db7670a889a5bdd8f7f3bf6125fb6ea14bc
kawakamir <rui.kawakami@gmail.com>
2018-08-01 19:54:26 +0900

    fix user controller first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def show
+    @name = current_user.name
+    @blogs = current_user.blogs
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
