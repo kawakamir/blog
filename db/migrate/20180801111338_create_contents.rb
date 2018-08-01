007f2e89aeff9dcfc0fc44b14030f86be661f321
kawakamir <rui.kawakami@gmail.com>
2018-08-01 20:22:56 +0900

    fix the db-parameter of contents-table

--- db/migrate/20180801111338_create_contents.rb
+++ db/migrate/20180801111338_create_contents.rb
@@ -1,7 +1,8 @@
 class CreateContents < ActiveRecord::Migration[5.0]
   def change
     create_table :contents do |t|
-
+      t.text :text
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
