c2e53f2702a35d5df4ab3ca817ff3491c2910650
kawakamir <rui.kawakami@gmail.com>
2018-08-02 12:45:41 +0900

    create view-of-index-content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,7 @@
 class ContentsController < ApplicationController
 
   def index
+    @contents = Content.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "contents#index"
   resources :users, only: [:index, :edit, :update] do
     resources :blogs, only: [:index, :create]
   end
