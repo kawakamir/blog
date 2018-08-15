883fdface4f7ff40340d6e61abf2f3c73fb8141e
kawakamir <rui.kawakami@gmail.com>
2018-08-15 19:45:58 +0900

    create path to edit-function and next update-function

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,8 +1,8 @@
 $(function(){
   $(document).on('click', 'a.edit', function() {
     var content_id, id;
-    id = $(this).attr('id');
-    content_id = $(this).attr('content_id');
+    id = $(this).parent().attr('id');
+    content_id = $(this).parent().attr('content_id');
     return $.get( content_id + "/comments/" + id + "/edit");
   });
 })
